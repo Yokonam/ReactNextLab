5ce741fea4b5044a0378509a93bec807d93835f6
Yokonam <s.yokonami@gmail.com>
2024-05-06 22:12:46 +0900

    feat: Update counter-button to use minCount and maxCount variables

--- counter-button/src/app/page.tsx
+++ counter-button/src/app/page.tsx
@@ -4,17 +4,18 @@ import { useState } from 'react'
 export default function Home() {
   const [count, setCount] = useState(0)
   const [history, setHistory] = useState<number[]>([])
-  const [countMax, setCountMax] = useState(10)
+  const [maxCount, setMaxCount] = useState(10)
+  const [minCount, setMinCount] = useState(0)
 
   function countPlus() {
-    if (count < 10) {
+    if (count < maxCount) {
       setCount(count + 1)
       setHistory([...history, count + 1])
     }
   }
 
   function countMinus() {
-    if (0 < count) {
+    if (minCount < count) {
       setCount(count - 1)
       setHistory([...history, count - 1])
     }
@@ -31,6 +32,16 @@ export default function Home() {
       <button onClick={countPlus}>カウンターを増やす</button>
       <button onClick={countMinus}>カウンターを減らす</button>
       <button onClick={handleClickReset}>カウンターをリセット</button>
+      <input
+        type="number"
+        value={maxCount}
+        onChange={(e) => setMaxCount(Number(e.target.value))}
+      />
+      <input
+        type="number"
+        value={minCount}
+        onChange={(e) => setMinCount(Number(e.target.value))}
+      />
       <ul>
         {history.map((item, index) => (
           <li key={index}>{item}</li>
