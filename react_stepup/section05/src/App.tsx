2234d92d31091a9ac7383a23c3d2488667637ea5
yokonam <s.yokonami@gmail.com>
2024-06-10 20:06:24 +0900

    feat: Add Home, Page1, and Page2 components

--- react_stepup/section05/src/App.tsx
+++ react_stepup/section05/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { Home } from './Home'
+import { Page1 } from './Page1'
+import { Page2 } from './Page2'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/page1">Page1</Link>
+      <br />
+      <Link to="/page2">Page2</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/page1" element={<Page1 />} />
+
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
