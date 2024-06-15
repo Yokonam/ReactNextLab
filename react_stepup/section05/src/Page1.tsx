eca0fcde3c7e66928c71e91737701bf44801cb6a
yokonam <s.yokonami@gmail.com>
2024-06-15 22:15:05 +0900

    feat: Update Page1 and Page2 routing

--- react_stepup/section05/src/Page1.tsx
+++ react_stepup/section05/src/Page1.tsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom'
+interface CustomLinkProps {
+  pathname: string
+  state: number[]
+}
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()]
   return (
     <>
       <h1>Page1</h1>
-      <Link to="/page1/DetailA">DetailA</Link>
+      <Link to={{ pathname: '/page1/detailA', state: arr } as CustomLinkProps}>
+        DetailA
+      </Link>
       <br />
       <Link to="/page1/DetailB">DetailB</Link>
     </>
--- react_stepup/section05/src/Page2.tsx
+++ react_stepup/section05/src/Page2.tsx
@@ -4,7 +4,7 @@ export const Page2 = () => {
   return (
     <>
       <h1>Page2</h1>
-      <Link to="000?name=a">URLパラメーターページ</Link>
+      <Link to="000?name=tomodachi">URLパラメーターページ</Link>
       <Outlet />
     </>
   )
