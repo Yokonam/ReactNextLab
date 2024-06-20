cc1c3b7f71001044a415a38f48edab017e861c1e
yokonam <s.yokonami@gmail.com>
2024-06-20 19:17:40 +0900

    feat: Add robots.txt file and PrimaryButton component

---
+++ react_stepup/section06/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { PrimaryButton } from './components/atoms/button/PrimaryBUtton';
+
+function App() {
+  return (
+    <div className="App">
+     <PrimaryButton>テスト</PrimaryButton>
+    </div>
+  );
+}
+
+export default App;
