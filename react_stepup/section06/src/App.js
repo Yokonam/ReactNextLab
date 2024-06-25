d0d037757c610c6efd999f2cd35197471e4ab3c1
yokonam <s.yokonami@gmail.com>
2024-06-25 18:19:51 +0900

    feat: Update UserCard component to display user information with styled dl

--- react_stepup/section06/src/App.js
+++ react_stepup/section06/src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user = {
+  name: "わんこ",
+  image: "https://placehold.jp/150x150.png",
+  email: "https://example.com",
+  phone: "000-0000-0000",
+  company: {
+    name: "テスト株式会社",
+    website: "https://example.com"
+  },
+}
 
 function App() {
   return (
@@ -12,6 +24,7 @@ function App() {
      <SecondaryButton>テスト</SecondaryButton>
     <br/>
     <SearchInput />
+    <UserCard user={user} />
     </div>
   );
 }
--- react_stepup/section06/src/components/organisms/user/UserCard.jsx
+++ react_stepup/section06/src/components/organisms/user/UserCard.jsx
@@ -1,16 +1,33 @@
+import styled from 'styled-components'
 export const UserCard = (props) => {
   const { user } = props
   return (
     <div>
       <img src={user.image} alt={user.name} />
       <p>{user.name}</p>
-      <dl>
-        <dt>メール</dt>
-        <dd>{user.email}</dd>
-        <dt>TEL</dt>
-        <dd>{user.phone}</dd>
-        <dt>会社名</dt>
-      </dl>
+      <SDl>
+        <dl>
+          <dt>メール</dt>
+          <dd>1</dd>
+          <dt>TEL</dt>
+          <dd>1</dd>
+          <dt>会社名</dt>
+        </dl>
+      </SDl>
     </div>
   )
 }
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0;
+  dl {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow: hidden;
+  }
+`
