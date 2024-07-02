edcd94378f692cac35fc0f0180c68ed5b7b9849a
yokonam <s.yokonami@gmail.com>
2024-07-02 20:08:53 +0900

    feat: Add lazy loading for images in Users page

--- react_stepup/section06/src/components/pages/Users.jsx
+++ react_stepup/section06/src/components/pages/Users.jsx
@@ -1,12 +1,44 @@
 import styled from 'styled-components'
+import { SearchInput } from '../molecules/SearchInput'
+import { UserCard } from '../organisms/user/UserCard'
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    name: `わんこ${val}`,
+    image: 'https://placehold.jp/150x150.png',
+    email: 'https://example.com',
+    phone: '000-0000-0000',
+    company: {
+      name: 'テスト株式会社',
+      website: 'https://example.com'
+    }
+  }
+})
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.name} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   )
 }
 
 const SContainer = styled.div`
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: 20px;
 `
--- react_stepup/section06/src/components/router/Router.jsx
+++ react_stepup/section06/src/components/router/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Top } from '../pages/Top'
 import { Users } from '../pages/Users'
 import { DefaultLayout } from '../templates/DefaultLayout'
+import { HeaderOnly } from '../templates/HeaderOnly'
 export const Router = () => {
   return (
     <BrowserRouter>
@@ -17,9 +18,9 @@ export const Router = () => {
         <Route
           path="/Users"
           element={
-            <DefaultLayout>
+            <HeaderOnly>
               <Users />
-            </DefaultLayout>
+            </HeaderOnly>
           }
         />
       </Routes>
