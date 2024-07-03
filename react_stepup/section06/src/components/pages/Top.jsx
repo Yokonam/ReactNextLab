41705f3b56e45584b67356301a7c8539c34b4048
yokonam <s.yokonami@gmail.com>
2024-07-03 20:55:53 +0900

    feat: Add buttons for admin and regular users in Top component

--- react_stepup/section06/src/components/pages/Top.jsx
+++ react_stepup/section06/src/components/pages/Top.jsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components'
+import { SecondaryButton } from '../atoms/button/SecondaryButton'
 export const Top = () => {
   return (
     <SContainer>
       <h2>top</h2>
+      <SecondaryButton>管理者</SecondaryButton>
+      <br />
+      <SecondaryButton>一般</SecondaryButton>
     </SContainer>
   )
 }
