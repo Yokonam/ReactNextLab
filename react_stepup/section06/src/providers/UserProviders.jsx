da06b7d5ac918707094d840bda6fd73feddd1bba
yokonam <s.yokonami@gmail.com>
2024-07-09 21:38:45 +0900

    feat: Update UserProvider to use useState for managing user info

--- react_stepup/section06/src/providers/UserProviders.jsx
+++ react_stepup/section06/src/providers/UserProviders.jsx
@@ -1,12 +1,12 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const UserContext = createContext({})
 
 export const UserProvider = (props) => {
   const { children } = props
-  const contextValue = 'Hello'
+  const [userInfo, setUserInfo] = useState(null)
   return (
-    <UserContext.Provider value={{ contextValue }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   )
