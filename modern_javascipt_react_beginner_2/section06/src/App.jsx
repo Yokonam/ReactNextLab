6d91a96589607517f20b208373fcbb40ba796c02
yokonam <s.yokonami@gmail.com>
2024-07-25 19:50:55 +0900

    feat: Add ColorfulMessage component for displaying colorful messages

--- modern_javascipt_react_beginner_2/section06/src/App.jsx
+++ modern_javascipt_react_beginner_2/section06/src/App.jsx
@@ -1,9 +1,12 @@
+import { ColorfulMessage } from './components/ColorfulMessage'
+
 export const App = () => {
   const onClickButton = () => alert('こんにちは')
   return (
     <div>
       <h1>こんにちは</h1>
       <p>お元気?w</p>
+      <ColorfulMessage color="blue">お元気?</ColorfulMessage>
       <button className="button" onClick={onClickButton}>
         ボタン
       </button>
