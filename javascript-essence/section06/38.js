5776d27bd81a4072987e52cc954fc4102884929a
yokonam <s.yokonami@gmail.com>
2024-08-31 21:03:16 +0900

    feat: Update title in section05/index.html and add section06/index.html
    - Update the title in section05/index.html to "section05"
    - Add section06/index.html with a basic HTML structure and a script tag for 38.js

---
+++ javascript-essence/section06/38.js
@@ -0,0 +1,27 @@
+// let a = 0;
+// let a = 0;
+
+// var b = 0;
+// var b = 0;
+
+let a = 0;
+a=1;
+
+const b = 0;
+// b = 1; // error
+
+{
+    let c = 0;
+    c = 1;
+    }
+
+    {
+    const d = 0;
+    // d = 1; // error
+}
+
+
+console.log(d);
+let d = 0; // error
+console.log(e);
+var e = 0;
