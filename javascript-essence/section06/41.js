71d0a53dd2810f238c23e30fc1c4d12806acf1ee
yokonam <s.yokonami@gmail.com>
2024-09-07 23:20:28 +0900

    feat: Add JavaScript code to compare variables
    Add a new JavaScript file, 41.js, in section06 that contains a function to compare variables using strict equality (===) and loose equality (==). The function is called with different variable values to demonstrate the comparison results. Also, update the index.html file in section06 to include the new JavaScript file.

---
+++ javascript-essence/section06/41.js
@@ -0,0 +1,18 @@
+function fn(a, b) {
+    console.log(a === b);
+    console.log(a == b);
+}
+
+let a = "1";
+let b = 1;
+
+let c = true;
+
+// fn(a, b)
+// fn(b, Number(c))
+
+let d = "";
+let e = 0;
+let f = "0";
+
+fn(d, e)
