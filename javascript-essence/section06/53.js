b5261470864809ef90003f7783e7409c704db889
yokonam <s.yokonami@gmail.com>
2024-10-23 18:38:28 +0900

    feat: Update 53.js to add console log examples for object properties and function arguments

--- javascript-essence/section06/53.js
+++ javascript-essence/section06/53.js
@@ -8,6 +8,8 @@ let val2 = val;
 val2 = 1;
 
 // console.log(val); // 問題１
+// 0
+// console.log(val);
 
 let obj = {
     prop1: 10
@@ -16,7 +18,8 @@ const obj2 = obj;
 obj2.prop1 = 9;
 
 // console.log(obj); // 問題２
-
+// { prop1: 9 }
+// console.log(obj);
 function fn1(arg) {
     arg.prop2 = 8;
     return arg;
@@ -24,7 +27,8 @@ function fn1(arg) {
 let obj3 = fn1(obj2);
 
 // console.log(obj); // 問題３
-
+// { prop1: 9, prop2: 8 }
+// console.log(obj);
 function fn2(arg) {
     arg = {};
     return arg;
@@ -32,3 +36,5 @@ function fn2(arg) {
 const obj4 = fn2(obj); // 問題４
 
 // console.log(obj); // 問題４
+// { prop1: 9, prop2: 8 }
+// console.log(obj);
