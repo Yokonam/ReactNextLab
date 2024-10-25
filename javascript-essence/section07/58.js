e6c0b49db59e175604777250077c3921e4884401
yokonam <s.yokonami@gmail.com>
2024-10-25 17:27:14 +0900

    feat: Refactor fn function to use arguments object and log parameters

--- javascript-essence/section07/58.js
+++ javascript-essence/section07/58.js
@@ -1,7 +1,11 @@
-function fn(a, b=1){
+function fn(){
+    let a = arguments[0];
+    let b = arguments[1];
+    console.log(arguments)
     console.log(a, b);
+    return a
 }
 
-fn(1, null)
-
+let c = fn(1, undefined);
+console.log(c);
 // 意図して空で渡したい場合はnullを渡す
