92112f4341b63bce77c7242cf11dd696e06fe58e
yokonam <s.yokonami@gmail.com>
2024-12-03 18:40:47 +0900

    feat: Update hello function to accept multiple parameters and demonstrate apply/call usage

--- javascript-essence/section07/65.js
+++ javascript-essence/section07/65.js
@@ -1,6 +1,24 @@
-function hello() {
-    console.log('hello ' + this.name);
+function hello(name, name2) {
+    console.log('hello ' + name + name2);
 
 }
 
-const b = hello.bind({name: 'Lee'});
+const lee = {name: 'Lee'};
+
+const b = hello.bind(lee);
+
+b(); // hello Lee
+
+hello.apply(lee, ['tom', 'bob']); // hello Lee
+hello.call(lee, 'tom', 'bob'); // hello Lee
+
+const array = [1, 2, 3, 4, 5];
+
+// const max = Math.max.apply(null, array);
+// console.log(max); // 5
+
+// const result = Math.max.apply(null, array); // 5
+
+// console.log(result)
+const result = Math.max(...array); // 5
+console.log(result)
